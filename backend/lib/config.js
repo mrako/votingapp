9378dca45e5b2f987e4d13b286f494d1ac535999
Marko Klemetti <marko.klemetti@gmail.com>
2016-04-16 10:17:00 +0300

    Updated urls to match docker compose

--- backend/lib/config.js
+++ backend/lib/config.js
@@ -5,7 +5,7 @@ module.exports = {
   KEYS: ['ffa35f3369218969482e8ad0da77a2f1'],
   DEFAULT_LIMIT: 100,
   COOKIE_MAX_AGE: 30 * 24 * 60 * 60 * 1000, // 30 days
-  DATABASE_URL : process.env.DATABASE_URL || 'postgres://votingapp:votingapp@localhost/votingapp',
+  DATABASE_URL : process.env.DATABASE_URL || 'postgres://votingapp:votingapp@db/votingapp',
   FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID || 'fake_app_id', //test app id
   FACEBOOK_APP_SECRET: process.env.FACEBOOK_APP_SECRET || 'fake_app_secret' //test app secret
 };
